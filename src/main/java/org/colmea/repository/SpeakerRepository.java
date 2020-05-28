43dccd37ac8c0c18b6437cfbce186343f0cba990
Arthur Schwaiger <a.schwaiger@evs.com>
2020-05-28 14:42:35 +0200

    Create blank Spring Boot app

--- src/main/java/org/colmea/repository/SpeakerRepository.java
+++
@@ -1,9 +0,0 @@
-package org.colmea.repository;
-
-import org.colmea.model.Speaker;
-
-import java.util.List;
-
-public interface SpeakerRepository {
-    List<Speaker> findAll();
-}
--- src/main/java/org/colmea/service/SpeakerService.java
+++
@@ -1,9 +0,0 @@
-package org.colmea.service;
-
-import org.colmea.model.Speaker;
-
-import java.util.List;
-
-public interface SpeakerService {
-    List<Speaker> findAll();
-}
